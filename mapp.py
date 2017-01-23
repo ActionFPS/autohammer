9723893434367d2fd35ae5d3b68dbe34639c937d
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-01-23 08:50:14 +0800

    Fix bug

--- mapp.py
+++ mapp.py
@@ -1,7 +1,9 @@
 import os
 import logic
 import tailer
-for line in tailer.follow(open(sys.getenv('SOURCE_FILE'))):
+sf = os.getenv('SOURCE_FILE')
+assert sf != None, "Set SOURCE_FILE env"
+for line in tailer.follow(open(sf)):
     call_command = logic.extract_command(line)
     if call_command is not None:
         os.system(call_command)
